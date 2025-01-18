a1c5ade010282c88fff3a6af05cc2d66c0bc0a28
Sreekumar <sharonbovas@gmail.com>
2025-01-18 11:26:58 +0400

    First Commit - Created XCode project

---
+++ SendMoney/SendMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendMoney
+//
+//  Created by Sharon Varghese on 17/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
